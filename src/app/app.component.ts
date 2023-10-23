5a5f38d9ededdd01eff7ea4a2d6606ad876a22c5
Agasthi98 <sankalana98@gmail.com>
2023-10-23 17:41:21 +0530

    angular test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test';
+
+  userForm: FormGroup;
+  submittedRecords: any[] = [];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.userForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      amount: ['', [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  /**
+   * 
+   * @returns submit filled data
+   */
+  onSubmit() {
+    if (this.userForm.invalid) {
+      return;
+    }
+
+    const record = this.userForm.value;
+    if (this.submittedRecords.some(r => r.email === record.email)) {
+      alert('Email already exist!');
+      return;
+    }
+
+    this.submittedRecords.push(record);
+    this.submittedRecords.sort((a, b) => b.amount - a.amount); // sort in descending order
+    this.userForm.reset();
+  }
+
+  /**
+   * 
+   * @param index Delete record from table
+   */
+  deleteRecord(index: number) {
+    this.submittedRecords.splice(index, 1);
+  }
+
+ 
 }
+
+
